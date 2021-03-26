93201a28d8b11bb57bda4a909f99420fee8b89d1
Gustavo Tonietto <tonietto@gmail.com>
2021-03-26 09:01:38 +0100

    Update custom  planck  qmk firmware

--- qmk_firmware/keyboards/planck/keymaps/tonietto/keymap.c
+++ qmk_firmware/keyboards/planck/keymaps/tonietto/keymap.c
@@ -53,7 +53,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,        KC_LALT, KC_LGUI, BACKLIT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, RGUI_T(KC_DOWN), RALT_T(KC_UP), RCTL_T(KC_RGHT)
 ),
 
-
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
