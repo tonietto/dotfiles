904fc4ed1a254170308cb53910be3828cb2ac5da
Gustavo Tonietto <tonietto@gmail.com>
2021-05-05 16:34:26 +0200

    Update planck keymap

--- qmk_firmware/keyboards/planck/keymaps/tonietto/keymap.c
+++ qmk_firmware/keyboards/planck/keymaps/tonietto/keymap.c
@@ -19,6 +19,7 @@
 
 
 enum planck_layers {
+  _WORKMAN,
   _QWERTY,
   _LOWER,
   _RAISE,
@@ -27,6 +28,7 @@ enum planck_layers {
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
+  WORKMAN,
   BACKLIT,
 };
 
@@ -34,6 +36,24 @@ enum planck_keycodes {
 #define RAISE MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Workman (https://workmanlayout.org)
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   D  |   R  |   W  |   B  |   J  |   F  |   U  |   P  |   ;  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   H  |   T  |   G  |   Y  |   N  |   E  |   O  |   I  |Enter |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   M  |   C  |   V  |   K  |   L  |   ,  |   .  |   /  |Shift |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  |GUI   | Brite|Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_WORKMAN] = LAYOUT_planck_grid(
+    KC_TAB,         KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P,            KC_SCLN,       KC_BSPC,
+    LCTL_T(KC_ESC), KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,            KC_I,          LSFT_T(KC_QUOT),
+    KC_LSFT,        KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L,    KC_COMM, KC_DOT,          KC_SLSH,       KC_ENT,
+    KC_LCTL,        KC_LALT, KC_LGUI, BACKLIT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, RALT_T(KC_DOWN), RGUI_T(KC_UP), RCTL_T(KC_RGHT)
+),
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
@@ -50,8 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,            KC_P,          KC_BSPC,
     LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,            KC_SCLN,       LSFT_T(KC_QUOT),
     KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,          KC_SLSH,       KC_ENT ,
-    KC_LCTL,        KC_LALT, KC_LGUI, BACKLIT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, RGUI_T(KC_DOWN), RALT_T(KC_UP), RCTL_T(KC_RGHT)
+    KC_LCTL,        KC_LALT, KC_LGUI, BACKLIT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, RALT_T(KC_DOWN), RGUI_T(KC_UP), RCTL_T(KC_RGHT)
 ),
+
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -94,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-| Eject|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |      |MUSmod|Aud on|Audoff|      |      |Qwerty|      |      |      |      |
+ * | Del  |      |MUSmod|Aud on|Audoff|      |      |Qwerty|Workmn|      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -103,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid(
     _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_EJCT,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, QWERTY,  _______,  _______,  _______,  _______,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, QWERTY,  WORKMAN,  _______, _______, _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
@@ -121,9 +142,16 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case WORKMAN:
+      if (record->event.pressed) {
+        print("mode just switched to workman\n");
+        set_single_persistent_default_layer(_WORKMAN);
+      }
+      return false;
+      break;
     case QWERTY:
       if (record->event.pressed) {
-        print("mode just switched to qwerty and this is a huge string\n");
+        print("mode just switched to qwerty\n");
         set_single_persistent_default_layer(_QWERTY);
       }
       return false;
